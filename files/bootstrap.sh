76a0b10648ce46d64d4c2514778b772aed9a73ee
Dennis Wagelaar <dennis.wagelaar@corilus.be>
2020-08-04 10:50:42 +0200

    General fixes + update to pypy3.6-v7.3.1

--- files/bootstrap.sh
+++ files/bootstrap.sh
@@ -1,23 +1,21 @@
 #/bin/bash
 
-set -e
+set -euo pipefail
 
 cd
 
-if [[ -e $HOME/.bootstrapped ]]; then
+if [[ -e ${HOME}/.bootstrapped ]]; then
   exit 0
 fi
 
-PYPY_VERSION=5.1.0
-
-if [[ -e $HOME/pypy-$PYPY_VERSION-linux64.tar.bz2 ]]; then
-  tar -xjf $HOME/pypy-$PYPY_VERSION-linux64.tar.bz2
-  rm -rf $HOME/pypy-$PYPY_VERSION-linux64.tar.bz2
+if [[ -e ${HOME}/pypy${PYTHON_VERSION}-v${PYPY_VERSION}-linux64.tar.bz2 ]]; then
+  tar -xjf ${HOME}/pypy${PYTHON_VERSION}-v${PYPY_VERSION}-linux64.tar.bz2
+  rm -rf ${HOME}/pypy${PYTHON_VERSION}-v${PYPY_VERSION}-linux64.tar.bz2
 else
-  wget -O - https://bitbucket.org/pypy/pypy/downloads/pypy-$PYPY_VERSION-linux64.tar.bz2 |tar -xjf -
+  wget -O - https://bitbucket.org/pypy/pypy/downloads/pypy${PYTHON_VERSION}-v${PYPY_VERSION}-linux64.tar.bz2 | tar -xjf -
 fi
 
-mv -n pypy-$PYPY_VERSION-linux64 pypy
+mv -n pypy${PYTHON_VERSION}-v${PYPY_VERSION}-linux64 pypy
 
 ## library fixup
 mkdir -p pypy/lib
