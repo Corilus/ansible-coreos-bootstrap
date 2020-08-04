2f2a313b429f793577c66858cce55ced50772fc4
Dennis Wagelaar <dennis.wagelaar@corilus.be>
2020-08-04 16:13:29 +0200

    Fix get-pip

--- files/bootstrap.sh
+++ files/bootstrap.sh
@@ -26,10 +26,8 @@ mkdir -p $HOME/bin
 
 cat > $HOME/bin/python <<EOF
 #!/bin/bash
-LD_LIBRARY_PATH=$HOME/pypy/lib:$LD_LIBRARY_PATH exec $HOME/pypy/bin/pypy3 "\$@"
+LD_LIBRARY_PATH=$HOME/pypy/lib:$LD_LIBRARY_PATH exec $HOME/pypy/bin/pypy "\$@"
 EOF
 
 chmod +x ${HOME}/bin/python
 ${HOME}/bin/python --version
-
-touch ${HOME}/.bootstrapped
