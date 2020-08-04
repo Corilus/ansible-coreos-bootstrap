f4b6793a9ae62e112a3e1e9aefdb5eacc8029398
Dennis Wagelaar <dennis.wagelaar@corilus.be>
2020-08-04 12:55:12 +0200

    Fixes for pypy3

--- files/bootstrap.sh
+++ files/bootstrap.sh
@@ -26,7 +26,7 @@ mkdir -p $HOME/bin
 
 cat > $HOME/bin/python <<EOF
 #!/bin/bash
-LD_LIBRARY_PATH=$HOME/pypy/lib:$LD_LIBRARY_PATH exec $HOME/pypy/bin/pypy "\$@"
+LD_LIBRARY_PATH=$HOME/pypy/lib:$LD_LIBRARY_PATH exec $HOME/pypy/bin/pypy3 "\$@"
 EOF
 
 chmod +x ${HOME}/bin/python
